3773a404c44d5cd855e5dc2a8521fe52b5259275
Aquafresh <choi.abri@gmail.com>
2020-05-28 10:13:29 +0900

    ApiExceptionSubscriber

--- src/EventListener/ApiExceptionSubscriber.php
+++ src/EventListener/ApiExceptionSubscriber.php
@@ -2,47 +2,29 @@
 
 namespace App\EventListener;
 
-use App\Api\ApiProblem;
-use App\Api\ApiProblemException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ApiExceptionSubscriber implements EventSubscriberInterface
 {
     public function onKernelException(ExceptionEvent $event)
     {
-        $e = get_class($event->getThrowable());
-
-        $statusCode = $e instanceof HttpExceptionInterface ? $e->getStatusCode() : 500;
-
-        // allow 500 errors to be thrown
-        if ($statusCode >= 500) {
-            return;
-        }
-
-        if ($e instanceof ApiProblemException) {
-            $apiProblem = $e->getApiProblem();
+        if ($event->getThrowable()->getCode() and $event->getThrowable()->getCode() > 200) {
+            $code = $event->getThrowable()->getCode();
+        } elseif (method_exists($event->getThrowable(), 'getStatusCode') == true) {
+            $code = $event->getThrowable()->getStatusCode();
         } else {
-            $apiProblem = new ApiProblem(
-                $statusCode
-            );
-
-            /*
-             * If it's an HttpException message (e.g. for 404, 403),
-             * we'll say as a rule that the exception message is safe
-             * for the client. Otherwise, it could be some sensitive
-             * low-level exception, which should *not* be exposed
-             */
-            if ($e instanceof HttpExceptionInterface) {
-                $apiProblem->set('detail', $e->getMessage());
-            }
+            $code = 404;
         }
 
-        $response = new JsonResponse(
-            $apiProblem->getStatusCode()
+        $message = [
+            'message' => $event->getThrowable()->getMessage(),
+            'code' => $code
+        ];
+
+        $response = new JsonResponse($message
         );
         $response->headers->set('Content-Type', 'application/problem+json');
 
