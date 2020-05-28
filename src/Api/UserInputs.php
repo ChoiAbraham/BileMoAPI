a3da375857fb98fc961ee6fc8a9e71acb8317c19
Aquafresh <choi.abri@gmail.com>
2020-05-28 09:13:43 +0900

    post_endpoint_user with exceptions

---
+++ src/Api/UserInputs.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Api;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class UserInputs
+{
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     */
+    private $lastName;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     */
+    private $firstName;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank()
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email."
+     * )
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     */
+    private $address;
+
+    /**
+     * @var string|null
+     */
+    private $country;
+
+    /**
+     * @var string|null
+     */
+    private $telephone;
+
+    /**
+     * @return string|null
+     */
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string|null $lastName
+     */
+    public function setLastName(?string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string|null $fistName
+     */
+    public function setFirstName(?string $fistName): void
+    {
+        $this->firstName = $fistName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string|null $address
+     */
+    public function setAddress(?string $address): void
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string|null $country
+     */
+    public function setCountry(?string $country): void
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * @param string|null $telephone
+     */
+    public function setTelephone(?string $telephone): void
+    {
+        $this->telephone = $telephone;
+    }
+}
---
+++ src/Domain/ApiHandlers/PostUserHandler.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Domain\ApiHandlers;
+
+use App\Api\ApiProblem;
+use App\Api\ApiProblemException;
+use App\Api\UserInputs;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class PostUserHandler
+{
+    /** @var SerializerInterface */
+    private $serializer;
+
+    /** @var TokenStorageInterface */
+    private $storage;
+
+    /** @var EntityManagerInterface */
+    private $manager;
+
+    /** @var UrlGeneratorInterface */
+    private $url;
+
+    /** @var ValidatorInterface */
+    private $validator;
+
+    /** @var UserRepository */
+    private $userRepository;
+
+    /**
+     * PostUserHandler constructor.
+     * @param SerializerInterface $serializer
+     * @param TokenStorageInterface $storage
+     * @param EntityManagerInterface $manager
+     * @param UrlGeneratorInterface $url
+     * @param ValidatorInterface $validator
+     * @param UserRepository $userRepository
+     */
+    public function __construct(SerializerInterface $serializer, TokenStorageInterface $storage, EntityManagerInterface $manager, UrlGeneratorInterface $url, ValidatorInterface $validator, UserRepository $userRepository)
+    {
+        $this->serializer = $serializer;
+        $this->storage = $storage;
+        $this->manager = $manager;
+        $this->url = $url;
+        $this->validator = $validator;
+        $this->userRepository = $userRepository;
+    }
+
+    public function handle(Request $request, UserInterface $client)
+    {
+        if ($client == null) {
+            throw new AccessDeniedHttpException('You can\'t add an user');
+        } elseif ($client->getId() != $request->attributes->get('client_id')) {
+            throw new AccessDeniedHttpException('wrong id');
+        }
+
+        /** @var UserInputs $input */
+        $input = $this->serializer->deserialize($request->getContent(), UserInputs::class, 'json');
+
+        // Properties "firstname", "lastname", "email" are required in json to be valid
+        if ($input->getEmail() == null || $input->getFirstName() == null || $input->getFirstName() == null) {
+            $apiProblem = new ApiProblem(Response::HTTP_BAD_REQUEST, ApiProblem::TYPE_INVALID_REQUEST_BODY_FORMAT);
+            throw new ApiProblemException($apiProblem);
+        }
+
+        // If the user already exist, throw exception
+        $user = $this->userRepository->findOneBy(['email' => $input->getEmail()]);
+        if($user) {
+            $apiProblem = new ApiProblem(Response::HTTP_CONFLICT, ApiProblem::TYPE_INVALID_USER_EMAIL);
+            throw new ApiProblemException($apiProblem);
+        }
+
+        // Errors
+        $errors = $this->validator->validate($input);
+        if ($errors->count() > 0) {
+            $errors = $this->getErrors($errors);
+
+            $apiProblem = new ApiProblem(
+                400,
+                ApiProblem::TYPE_VALIDATION_ERROR
+            );
+            $apiProblem->set('errors', $errors);
+
+            throw new ApiProblemException($apiProblem);
+        }
+
+        return $this->hydrate($input);
+    }
+
+    public function hydrate(UserInputs $input)
+    {
+        $user = new User();
+        $user->setClient($this->storage->getToken()->getUser());
+        $user->setEmail($input->getEmail());
+        $user->setLastName($input->getLastname());
+        $user->setFirstName($input->getFirstName());
+        $user->setAddress($input->getAddress());
+        $user->setCountry($input->getCountry());
+        $user->setTelephone($input->getTelephone());
+
+        $this->manager->persist($user);
+        $this->manager->flush();
+
+        return [
+            "url" => $this->url->generate(
+                'get_user_details',
+                [
+                    'id' => $user->getId(),
+                    'client_id' => $this->storage->getToken()->getUser()->getId()
+                ]
+            )
+        ];
+    }
+
+    private function getErrors($errors)
+    {
+        foreach ($errors as $error) {
+            /** @var ConstraintViolation $error */
+            $data['errors'][] = $error->getPropertyPath() . ' => ' . $error->getMessage();
+        }
+
+        return $data;
+    }
+}
